e8215505872488ca0954c8dbe8896f9abf8170bf
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-17 18:30:05 -0700

    add expenses

--- src/App.js
+++ src/App.js
@@ -11,14 +11,58 @@ const initialExpenses = [
   { id: uuidv4(), charge: 'credit card', amount: 1200 },
 ];
 function App() {
+  // ******* state values ******
+  //all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
+  //single expense
+  const [charge, setCharge] = useState('');
+  //single amount
+  const [amount, setAmount] = useState('');
+  //alert
+  const [alert, setAlert] = useState({ show: false });
+  // ******* functionality ******
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 5000);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== '' && amount > 0) {
+      const singleExpense = { id: uuidv4(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      handleAlert({ type: 'success', text: 'item added' });
+      setCharge('');
+      setAmount('');
+    } else {
+      handleAlert({
+        type: 'danger',
+        text: `charge can't be empty value and amount value has to be greater than 0`,
+      });
+    }
+  };
+  //clear all items
 
   return (
     <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <Alert />
       <h1>budget calculator</h1>
       <main className='App'>
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
@@ -26,7 +70,7 @@ function App() {
         <span className='total'>
           ${' '}
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
