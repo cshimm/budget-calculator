f0389488d077c50303d2335d6c7f4e9901eca761
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-17 17:17:41 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
+import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
 
+const initialExpenses = [
+  { id: uuidv4(), charge: 'rent', amount: 1600 },
+  { id: uuidv4(), charge: 'car payment', amount: 400 },
+  { id: uuidv4(), charge: 'credit card', amount: 1200 },
+];
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className='App'>
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending :{' '}
+        <span className='total'>
+          ${' '}
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
+    </>
   );
 }
 
